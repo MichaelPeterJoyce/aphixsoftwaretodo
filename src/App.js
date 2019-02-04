d92a3cc3a22f523bf1e9f13946dec103b6a31d2c
michael.peter.joyce@gmail.com <michael.peter.joyce@gmail.com>
2019-02-04 23:17:41 +0000

    Small styling issue with background resolved and moved container down slightly which looks better

--- src/App.js
+++ src/App.js
@@ -3,17 +3,16 @@ import './App.scss';
 import AddToDoItem from "./containers/AddToDoItem/AddToDoItem";
 import FilteredToDoItems from "./containers/FilteredToDoItems/FilteredToDoItems";
 import Undo from "./containers/undo";
+
 class App extends Component {
     render() {
         return (
-            <div className='background-body'>
-                <div className="App container">
-                    <img alt='logo' src={require('./assets/img/logo.png')} className='img-fluid'/>
-                    <h3>Todo List Application Demo for Aphix Software</h3>
-                    <AddToDoItem/>
-                    <Undo/>
-                    <FilteredToDoItems/>
-                </div>
+            <div className="App container mt-5">
+                <img alt='logo' src={require('./assets/img/logo.png')} className='img-fluid'/>
+                <h3>Todo List Application Demo for Aphix Software</h3>
+                <AddToDoItem/>
+                <Undo/>
+                <FilteredToDoItems/>
             </div>
         );
     }
